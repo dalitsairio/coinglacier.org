5b3f4df19c9e33feb7092ab28f12be87e1f1b344
Dalit Sairio <dalit.sairio@protonmail.com>
2018-03-27 16:12:14 +0200

    refactored function createP2PKHaddresses()

--- src/js/bitcoin.js
+++ src/js/bitcoin.js
@@ -39,99 +39,114 @@ function createP2PKHaddresses (accounts, targetNetwork) {
         targetNetwork = bitcoinjs.networks.bitcoin.p2wpkhInP2sh;
     }
 
-    // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-    var mainnetORtestnet = 0;
-    if(targetNetwork === bitcoinjs.networks.testnet ||
-        targetNetwork === bitcoinjs.networks.testnet.p2wpkhInP2sh ||
-        targetNetwork === bitcoinjs.networks.testnet.p2wpkh){
-        mainnetORtestnet = 1;
-    }
-
     var result = [];
 
     // calculate addresses
     for(var accIndex = 0; accIndex < accounts.length; accIndex++) {
         var amount = accounts[accIndex];
 
-        result[accIndex] = [];
-        result[accIndex]['credentials'] = [];
-        var accountPath;
+        var account =  root.derivePath(getAccountPath(targetNetwork, accIndex));
+        account.keyPair.network = targetNetwork;
 
-        // get extended public key of each account
-        switch (targetNetwork) {
+        result[accIndex] = [];
+        result[accIndex]['credentials'] = createAccountCredentials(account, targetNetwork, amount);
+        result[accIndex]['xpub'] = account.neutered().toBase58();
+
+    }
+
+    return result;
+}
+
+// creates addresses and privKeys on the level of a BIP32 account
+function createAccountCredentials(account, network, amount){
+
+    var credentials = [];
+
+    for (var index = 0; index < amount; index++) {
+
+        // PrivKey / BIP44 | BIP49 | BIP84 / Bitcoin | Testnet / Account / External / First Address
+        var addressPath = "0/" + index;
+        var bip32 = account.derivePath(addressPath);
+        bip32.keyPair.network = network;
+
+        switch (network) {
             case bitcoinjs.networks.bitcoin:
             case bitcoinjs.networks.testnet:
 
-                // PrivKey / BIP44 / Bitcoin | Testnet / Account
-                accountPath = "m/44'/" + mainnetORtestnet + "'/" + accIndex + "'";
-
+                credentials.push({privateKey: bip32.keyPair.toWIF(), address: bip32.getAddress()});
                 break;
             case bitcoinjs.networks.bitcoin.p2wpkhInP2sh:
             case bitcoinjs.networks.testnet.p2wpkhInP2sh:
 
-                // PrivKey / BIP49 / Bitcoin | Testnet / Account
-                accountPath = "m/49'/" + mainnetORtestnet + "'/" + accIndex + "'";
+                var pubKey = bip32.getPublicKeyBuffer();
+                var redeemScript = bitcoinjs.script.witnessPubKeyHash.output.encode(bitcoinjs.crypto.hash160(pubKey));
+                var scriptPubKey = bitcoinjs.script.scriptHash.output.encode(bitcoinjs.crypto.hash160(redeemScript));
 
+                credentials.push({
+                    privateKey: bip32.keyPair.toWIF(),
+                    address: bitcoinjs.address.fromOutputScript(scriptPubKey, network)
+                });
                 break;
             case bitcoinjs.networks.bitcoin.p2wpkh:
             case bitcoinjs.networks.testnet.p2wpkh:
 
-                // PrivKey / BIP84 / Bitcoin | Testnet / Account
-                accountPath = "m/84'/" + mainnetORtestnet + "'/" + accIndex + "'";
+                var pubKey = bip32.getPublicKeyBuffer();
+                var scriptPubKey = bitcoinjs.script.witnessPubKeyHash.output.encode(bitcoinjs.crypto.hash160(pubKey));
 
+                credentials.push({
+                    privateKey: bip32.keyPair.toWIF(),
+                    address: bitcoinjs.address.fromOutputScript(scriptPubKey, network)
+                });
                 break;
             default:
                 throw ("given network is not a valid network");
         }
-
-        var account =  root.derivePath(accountPath);
-        account.keyPair.network = targetNetwork;
-        result[accIndex]['xpub'] = account.neutered().toBase58();
-
-        for (var index = 0; index < amount; index++) {
-
-            // PrivKey / BIP44 | BIP49 | BIP84 / Bitcoin | Testnet / Account / External / First Address
-            var addressPath = "0/" + index;
-            var bip32 = account.derivePath(addressPath);
-
-            bip32.keyPair.network = targetNetwork;
-
-            switch (targetNetwork) {
-                case bitcoinjs.networks.bitcoin:
-                case bitcoinjs.networks.testnet:
-
-                    result[accIndex]['credentials'].push({privateKey: bip32.keyPair.toWIF(), address: bip32.getAddress()});
-                    break;
-                case bitcoinjs.networks.bitcoin.p2wpkhInP2sh:
-                case bitcoinjs.networks.testnet.p2wpkhInP2sh:
-
-                    var pubKey = bip32.getPublicKeyBuffer();
-                    var redeemScript = bitcoinjs.script.witnessPubKeyHash.output.encode(bitcoinjs.crypto.hash160(pubKey));
-                    var scriptPubKey = bitcoinjs.script.scriptHash.output.encode(bitcoinjs.crypto.hash160(redeemScript));
-
-                    result[accIndex]['credentials'].push({
-                        privateKey: bip32.keyPair.toWIF(),
-                        address: bitcoinjs.address.fromOutputScript(scriptPubKey, targetNetwork)
-                    });
-                    break;
-                case bitcoinjs.networks.bitcoin.p2wpkh:
-                case bitcoinjs.networks.testnet.p2wpkh:
-
-                    var pubKey = bip32.getPublicKeyBuffer();
-                    var scriptPubKey = bitcoinjs.script.witnessPubKeyHash.output.encode(bitcoinjs.crypto.hash160(pubKey));
-
-                    result[accIndex]['credentials'].push({
-                        privateKey: bip32.keyPair.toWIF(),
-                        address: bitcoinjs.address.fromOutputScript(scriptPubKey, targetNetwork)
-                    });
-                    break;
-                default:
-                    throw ("given network is not a valid network");
-            }
-        }
     }
 
-    return result;
+    return credentials;
+
+}
+
+function getBip44testnetOrMainnet(network){
+
+    if(network === bitcoinjs.networks.testnet ||
+        network === bitcoinjs.networks.testnet.p2wpkhInP2sh ||
+        network === bitcoinjs.networks.testnet.p2wpkh){
+
+        return 1;
+    }
+
+    return 0;
+}
+
+function getAccountPath(network, accountIndex){
+
+    // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+    var mainnetORtestnet = getBip44testnetOrMainnet(network);
+
+    // get extended public key of each account
+    switch (network) {
+        case bitcoinjs.networks.bitcoin:
+        case bitcoinjs.networks.testnet:
+
+            // PrivKey / BIP44 / Bitcoin | Testnet / Account
+            return "m/44'/" + mainnetORtestnet + "'/" + accountIndex + "'";
+
+        case bitcoinjs.networks.bitcoin.p2wpkhInP2sh:
+        case bitcoinjs.networks.testnet.p2wpkhInP2sh:
+
+            // PrivKey / BIP49 / Bitcoin | Testnet / Account
+            return "m/49'/" + mainnetORtestnet + "'/" + accountIndex + "'";
+
+        case bitcoinjs.networks.bitcoin.p2wpkh:
+        case bitcoinjs.networks.testnet.p2wpkh:
+
+            // PrivKey / BIP84 / Bitcoin | Testnet / Account
+            return "m/84'/" + mainnetORtestnet + "'/" + accountIndex + "'";
+
+        default:
+            throw ("given network is not a valid network");
+    }
 }
 
 module.exports = {
