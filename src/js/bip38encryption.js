f815be638c43243d4854ac65ca6fb22e665e97a3
Dalit Sairio <dalit.sairio@protonmail.com>
2018-05-14 20:37:49 +0200

    [FEAT] Adding BIP38 decryption function

--- src/js/bip38encryption.js
+++ src/js/bip38encryption.js
@@ -8,6 +8,14 @@ function encryptPrivKey(privKey, password) {
     return bip38.encrypt(decoded.privateKey, decoded.compressed, password);
 }
 
+// privKey must be a string beginning with a 6
+function decryptPrivKey(privKey_encrypted, password) {
+
+    var decryptedKey = bip38.decrypt(privKey_encrypted, password);
+    return wif.encode(0x80, decryptedKey.privateKey, decryptedKey.compressed);
+}
+
 module.exports = {
-    encryptPrivKey: encryptPrivKey
+    encryptPrivKey: encryptPrivKey,
+    decryptPrivKey: decryptPrivKey
 };
